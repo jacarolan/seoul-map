baf03adbe7c3dc85e5150c25ab341d818b441512
jacarolan <jacarolan22@gmail.com>
2019-01-07 19:21:38 -0600

    Restructured directories, added region drawing functionality

--- main.py
+++
@@ -1,4 +0,0 @@
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-
-print "hello world"
--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 
-print "hello world"
+
+def main():
+
+    baseMap = Image.open("data/SeoulTemplate.png")
+    baseMap.convert("RGBA")
+
+    baseMap = drawRegion(baseMap, 5, (0, 255, 0))
+
+    baseMap.show()
+
+def drawRegion(map, region, fill):
+    
+    data = np.array(map)
+    r, g, b, a = data.T
+
+    colorAreas = (r == 255) & (g == region) & (b == 255)
+    data[..., :-1][colorAreas.T] = fill
+
+    return Image.fromarray(data)
+
+
+main()
